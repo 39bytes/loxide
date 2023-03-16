684b932315db3b7b0f04e5189ede14cc7821ae8f
JeffDotPng <zhangjeff2014@gmail.com>
2023-03-16 08:57:03 -0400

    fix macro

--- src/parser/expr.rs
+++ src/parser/expr.rs
@@ -5,13 +5,14 @@ use crate::scanner::token::Token;
 macro_rules! parenthesize {
     ( $name:expr, $($e:expr), *) => {{
         let mut result = String::from("(");
-        result.push_str($name);
+        result.push_str(&$name.to_string());
         $(
             result.push(' ');
-            result.push_str($e);
+            result.push_str(&$e.to_string());
         )*
+        result.push(')');
         result
-    }}
+    }};
 }
 
 pub enum Expr {
@@ -32,7 +33,6 @@ pub enum Expr {
     },
 }
 
-// TODO: Refactor into macro
 impl Display for Expr {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         let text = match self {
@@ -40,10 +40,10 @@ impl Display for Expr {
                 left,
                 operator,
                 right,
-            } => format!("({} {} {})", operator.lexeme, left, right),
-            Expr::Grouping { expression } => format!("(group {})", expression),
+            } => parenthesize!(operator.lexeme, left, right),
+            Expr::Grouping { expression } => parenthesize!("group", expression),
             Expr::Literal { value } => value.to_string(),
-            Expr::Unary { operator, right } => format!("({} {})", operator.lexeme, right),
+            Expr::Unary { operator, right } => parenthesize!(operator.lexeme, right),
         };
         write!(f, "{text}")
     }
